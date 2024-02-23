9ec0e457e0499e66fbe4c60440084c44be073f5d
dhyana <dhyana.ixchel@gmail.com>
2024-02-22 18:19:07 -0600

    Se agregó la funcionalidad de que al presionar enter o en el icono de la lupa te redirija al inicio y se guarde la palabra en el session storage para en un futuro buscarla en la sección de productos

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*Secion de la barra de navegacion */
+let divBusqueda = document.getElementById("divBusqueda");
+let btnBuscar = document.getElementById("btnBuscar");
+let btnCancelarBusqueda = document.getElementById("CancelarBusqueda");
+let txtBusqueda = document.getElementById("txtBusqueda");
+let anchoPantalla = window.innerWidth;
+
+btnBuscar.addEventListener("click", function(event){
+    anchoPantalla = window.innerWidth;
+    txtBusqueda.value = txtBusqueda.value.trim();
+    if (txtBusqueda.value.length == 0 && txtBusqueda.clientWidth <= 10){
+        divBusqueda.style.width = "200px";
+        divBusqueda.style.marginRight = "29px";
+    } else if(txtBusqueda.value.length == 0 && txtBusqueda.clientWidth >= 190 ){
+        divBusqueda.style.width = "0px";
+        divBusqueda.style.marginRight = "1px"
+        txtBusqueda.value = "";
+    } else {
+        
+        console.log("se mandara a buscar el producto y se ira a la pagina de tienda");
+        sessionStorage.setItem("palabraBuscada", txtBusqueda.value);
+        txtBusqueda.value = txtBusqueda.value.trim();
+        txtBusqueda.value = "";
+        location.href = "index.html";
+        
+        
+    }
+})
+
+txtBusqueda.addEventListener("keyup", function(event){
+    if(event.code == 'Enter' && txtBusqueda.value.length != 0){
+        location.href = "index.html";
+        sessionStorage.setItem("palabraBuscada", txtBusqueda.value);
+    }
+});
+
+btnCancelarBusqueda.addEventListener("click",function(event){
+    divBusqueda.style.width = "0px";
+    divBusqueda.style.marginRight = "1px"
+    txtBusqueda.value = txtBusqueda.value.trim();
+    txtBusqueda.value = "";
+})
+/*Secion de la barra de navegacion */
