a3b2a6172b70e87d561172ab2ee7a28427d02b9c
ezaleta <egonzalezzaleta@gmail.com>
2024-02-27 14:18:08 -0600

    Se hicieron ajustes al navbar en su version normal y responsiva, modificando su estructura en HTML y estilo en CSS y JS. Los cambios fueron aplicados a todas las paginas. AABN ROMA.

--- js/main.js
+++ js/main.js
@@ -5,39 +5,55 @@ let btnCancelarBusqueda = document.getElementById("CancelarBusqueda");
 let txtBusqueda = document.getElementById("txtBusqueda");
 let anchoPantalla = window.innerWidth;
 
-btnBuscar.addEventListener("click", function(event){
+let navbar = document.getElementById("navbar");
+let navIcons = document.getElementById("navIcons");
+let navMenu = document.getElementById("navMenu");
+let searchField = document.getElementById("searchField");
+let offcanvasbody = document.getElementById("offcanvas-body");
+let header = document.getElementById("header");
+
+btnBuscar.addEventListener("click", function (event) {
     anchoPantalla = window.innerWidth;
     txtBusqueda.value = txtBusqueda.value.trim();
-    if (txtBusqueda.value.length == 0 && txtBusqueda.clientWidth <= 10){
+    if (txtBusqueda.value.length == 0 && txtBusqueda.clientWidth <= 10) {
         divBusqueda.style.width = "200px";
         divBusqueda.style.marginRight = "29px";
-    } else if(txtBusqueda.value.length == 0 && txtBusqueda.clientWidth >= 190 ){
+    } else if (txtBusqueda.value.length == 0 && txtBusqueda.clientWidth >= 190) {
         divBusqueda.style.width = "0px";
-        divBusqueda.style.marginRight = "1px"
+        divBusqueda.style.marginRight = "1px";
         txtBusqueda.value = "";
     } else {
-        
         console.log("se mandara a buscar el producto y se ira a la pagina de tienda");
         sessionStorage.setItem("palabraBuscada", txtBusqueda.value);
         txtBusqueda.value = txtBusqueda.value.trim();
         txtBusqueda.value = "";
         location.href = "tienda.html";
-        
-        
     }
-})
+});
 
-txtBusqueda.addEventListener("keyup", function(event){
-    if(event.code == 'Enter' && txtBusqueda.value.length != 0){
+txtBusqueda.addEventListener("keyup", function (event) {
+    if (event.code == "Enter" && txtBusqueda.value.length != 0) {
         location.href = "tienda.html";
         sessionStorage.setItem("palabraBuscada", txtBusqueda.value);
     }
 });
 
-btnCancelarBusqueda.addEventListener("click",function(event){
+if (window.matchMedia("(max-width: 425px)").matches) {
+    navbar.appendChild(searchField);
+    navbar.insertBefore(searchField, navbar.children[1]);
+    offcanvasbody.appendChild(navIcons);
+    offcanvasbody.appendChild(navMenu);
+} else {
+    navIcons.appendChild(searchField);
+    navIcons.insertBefore(searchField, navIcons.children[0]);
+    navbar.appendChild(navMenu);
+    navbar.appendChild(navIcons);
+}
+
+/* btnCancelarBusqueda.addEventListener("click", function (event) {
     divBusqueda.style.width = "0px";
-    divBusqueda.style.marginRight = "1px"
+    divBusqueda.style.marginRight = "1px";
     txtBusqueda.value = txtBusqueda.value.trim();
     txtBusqueda.value = "";
-})
+}); */
 /*Secion de la barra de navegacion */
