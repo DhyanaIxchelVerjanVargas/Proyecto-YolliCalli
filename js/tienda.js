d0307686f5dffff053f77cd7a237b92bbc52703e
Ollincatl <ollincatlj@gmail.com>
2024-02-29 18:31:10 -0600

    se agrego el codigo en JavaScript para que el producto publicado aparezca en el catalogo de la tienda

--- js/tienda.js
+++ js/tienda.js
@@ -11,7 +11,7 @@ let btnLimpiarFiltros = document.getElementById("btnLimpiarFiltros");
 let btnBusqueda = document.getElementById("btnBusqueda");
 let inputBusqueda = document.getElementById("inputBusqueda");
 let productos = new Array();
-let nuevosProductos = new Array();
+let productosNuevos = new Array();
 let auxBusqueda = new Array();
 let productosBuscados = new Array();
 
@@ -39,42 +39,42 @@ let productosBuscados = new Array();
     nombre="";
     descripcion="";
     precio = 0;
-    categorias=""
-    etiquetas="";
+    categoria = "";
+    etiquetas = "";
     imagen =""
-    //elaboradoPor ="";
+    // elaboradoPor ="";
     id="";
     static total=0;
 
-    constructor(nombre,descripcion,precio,categorias, etiquedas,imagen){
+    constructor(nombre,descripcion,precio, categoria, etiquetas, imagen){
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
-        //this.elaboradoPor = elaboradoPor;
-        this.categorias = categorias;
-        this.etiquetas = etiquedas;
-        this.imagen = imagen
+        // this.elaboradoPor = elaboradoPor;
+        this.categoria = categoria;
+        this.etiquetas = etiquetas;
+        this.imagen = imagen;
         Producto.total +=1;
         this.id= "CP" + Producto.total;
     }
  }
 
  function crearPropuctos(){
-    productos.push(new Producto("Alebrije Buho","Cojín bordado por artesanos de Tenango","https://programadestinosmexico.com/wp-content/uploads/2023/08/ARTESANIAS-OAX.jpg",200,"Artesanos de Tenango"));
-    productos.push(new Producto("Alebrije Ajolote","Cojín bordado por artesanos de Oaxaca","https://media.licdn.com/dms/image/D5612AQFx8phwV2yN6g/article-cover_image-shrink_720_1280/0/1697657051080?e=2147483647&v=beta&t=H57I5dr3zSXi79Rjsc5iTNgOweSYmRbCZw5eWzfAiVA",300,"Artesanos de Oaxaca"));
-    productos.push(new Producto("Cojin Tenango","Cojín bordado por artesanos de Chiapas","./src/img/productos/cojinTenango.png",400,"Artesanos de Chiapas"));
-    productos.push(new Producto("Muñeca lele","Cojín bordado por artesanos de Guerrero","https://www.turismomexico.es/wp-content/uploads/2019/01/munecas_mexicanas.jpg",500,"Artesanos de Guerrero"));
-    productos.push(new Producto("Florero Artesenal","Cojín bordado por artesanos de Tabasco","https://http2.mlstatic.com/D_NQ_NP_708931-MLM71088880686_082023-O.webp",600,"Artesanos de Tabasco"));
-    productos.push(new Producto("Mezcal en olla de barro","Cojín bordado por artesanos de Merida","https://i.pinimg.com/originals/fb/c4/1a/fbc41ad7a84bf6788e0e9ba69140dddd.jpg",700,"Artesanos de Merida"));
-    productos.push(new Producto("Alegria de amaranto","Cojín bordado por artesanos de los Mochis","https://biotrendies.com/wp-content/uploads/2015/07/alegria-amaranto.jpg",800,"Artesanos de los Mochis"));
-    productos.push(new Producto("Alebrije Ajolote","Cojín bordado por artesanos de Oaxaca","https://media.licdn.com/dms/image/D5612AQFx8phwV2yN6g/article-cover_image-shrink_720_1280/0/1697657051080?e=2147483647&v=beta&t=H57I5dr3zSXi79Rjsc5iTNgOweSYmRbCZw5eWzfAiVA",900,"Artesanos de Oaxaca"));
-    productos.push(new Producto("Muñeca lele","Cojín bordado por artesanos de Veracruz","https://www.turismomexico.es/wp-content/uploads/2019/01/munecas_mexicanas.jpg",1000,"Artesanos de Veracruz"));
-    productos.push(new Producto("Florero Artesenal","Cojín bordado por artesanos de Puebla","https://http2.mlstatic.com/D_NQ_NP_708931-MLM71088880686_082023-O.webp",1100,"Artesanos de Puebla"));
-    productos.push(new Producto("Mezcal en olla de barro","Cojín bordado por artesanos de Hidalgo","https://i.pinimg.com/originals/fb/c4/1a/fbc41ad7a84bf6788e0e9ba69140dddd.jpg",1200,"Artesanos de Hidalgo"));
-    productos.push(new Producto("Mantel de Oaxaca","Cojín bordado por artesanos de Oaxaca","https://oaxacaxamor.com/cdn/shop/products/Mantel_AzulDorado_Tlalixtac_9414_800x.jpg?v=1612829162",1300,"Artesanos de Oaxaca"));
-    productos.push(new Producto("Servilleta bordada","Cojín bordado por artesanos de Jalisco","https://www.mexicodesconocido.com.mx/wp-content/uploads/2023/02/servilletas-bodadas-mano-2-900x861.jpg",1400,"Artesanos de Jalisco"));
-    productos.push(new Producto("Carrito de juguete","Cojín bordado por artesanos de San Luis Potosi","https://http2.mlstatic.com/D_NQ_NP_845988-MLM53611700096_022023-O.webp",1500,"Artesanos de San Luis Potosi"));
-    productos.push(new Producto("Cojin Tenango","Cojín bordado por artesanos de Hermosillo","./src/img/productos/cojinTenango.png",1600,"Artesanos de Hermosillo"));
+    productos.push(new Producto("Alebrije Buho","Cojín bordado por artesanos de Tenango",200,"ropa","accesorio","https://programadestinosmexico.com/wp-content/uploads/2023/08/ARTESANIAS-OAX.jpg"));
+    productos.push(new Producto("Alebrije Ajolote","Cojín bordado por artesanos de Oaxaca",200,"ropa","accesorio","https://media.licdn.com/dms/image/D5612AQFx8phwV2yN6g/article-cover_image-shrink_720_1280/0/1697657051080?e=2147483647&v=beta&t=H57I5dr3zSXi79Rjsc5iTNgOweSYmRbCZw5eWzfAiVA"));
+    productos.push(new Producto("Cojin Tenango","Cojín bordado por artesanos de Chiapas",200,"ropa","accesorio","./src/img/productos/cojinTenango.png"));
+    productos.push(new Producto("Muñeca lele","Cojín bordado por artesanos de Guerrero",200,"ropa","accesorio","https://www.turismomexico.es/wp-content/uploads/2019/01/munecas_mexicanas.jpg"));
+    productos.push(new Producto("Florero Artesenal","Cojín bordado por artesanos de Tabasco",200,"ropa","accesorio","https://http2.mlstatic.com/D_NQ_NP_708931-MLM71088880686_082023-O.webp"));
+    productos.push(new Producto("Mezcal en olla de barro","Cojín bordado por artesanos de Merida",200,"ropa","accesorio","https://i.pinimg.com/originals/fb/c4/1a/fbc41ad7a84bf6788e0e9ba69140dddd.jpg"));
+    productos.push(new Producto("Alegria de amaranto","Cojín bordado por artesanos de los Mochis",200,"ropa","accesorio","https://biotrendies.com/wp-content/uploads/2015/07/alegria-amaranto.jpg"));
+    productos.push(new Producto("Alebrije Ajolote","Cojín bordado por artesanos de Oaxaca",200,"ropa","accesorio","https://media.licdn.com/dms/image/D5612AQFx8phwV2yN6g/article-cover_image-shrink_720_1280/0/1697657051080?e=2147483647&v=beta&t=H57I5dr3zSXi79Rjsc5iTNgOweSYmRbCZw5eWzfAiVA"));
+    productos.push(new Producto("Muñeca lele","Cojín bordado por artesanos de Veracruz",200,"ropa","accesorio","https://www.turismomexico.es/wp-content/uploads/2019/01/munecas_mexicanas.jpg"));
+    productos.push(new Producto("Florero Artesenal","Cojín bordado por artesanos de Puebla",200,"ropa","accesorio","https://http2.mlstatic.com/D_NQ_NP_708931-MLM71088880686_082023-O.webp"));
+    productos.push(new Producto("Mezcal en olla de barro","Cojín bordado por artesanos de Hidalgo",200,"ropa","accesorio","https://i.pinimg.com/originals/fb/c4/1a/fbc41ad7a84bf6788e0e9ba69140dddd.jpg"));
+    productos.push(new Producto("Mantel de Oaxaca","Cojín bordado por artesanos de Oaxaca",200,"ropa","accesorio","https://oaxacaxamor.com/cdn/shop/products/Mantel_AzulDorado_Tlalixtac_9414_800x.jpg?v=1612829162"));
+    productos.push(new Producto("Servilleta bordada","Cojín bordado por artesanos de Jalisco",200,"ropa","accesorio","https://www.mexicodesconocido.com.mx/wp-content/uploads/2023/02/servilletas-bodadas-mano-2-900x861.jpg"));
+    productos.push(new Producto("Carrito de juguete","Cojín bordado por artesanos de San Luis Potosi",200,"ropa","accesorio","https://http2.mlstatic.com/D_NQ_NP_845988-MLM53611700096_022023-O.webp"));
+    productos.push(new Producto("Cojin Tenango","Cojín bordado por artesanos de Hermosillo",200,"ropa","accesorio","./src/img/productos/cojinTenango.png"));
     
  }
 
@@ -182,8 +182,12 @@ function busquedaProducto(productoBuscar){
         crearPropuctos();
 
         if(this.localStorage.getItem("productosNuevos") != null){
-            nuevosProductos = JSON.parse(this.localStorage.getItem("productosNuevos"));
-            nuevosProductos.forEach((nuevoProducto)=>{
+        
+            
+            productosNuevos = JSON.parse(this.localStorage.getItem("productosNuevos"));
+            console.log("revisando la local store 1")
+            console.log(productosNuevos)
+            productosNuevos.forEach((nuevoProducto)=>{
                 productos.push(nuevoProducto);
             })
         }
@@ -192,8 +196,12 @@ function busquedaProducto(productoBuscar){
     }else{
         productos = JSON.parse(this.localStorage.getItem("productos"));
         if(this.localStorage.getItem("productosNuevos") != null){
-            nuevosProductos = JSON.parse(this.localStorage.getItem("productosNuevos"));
-            nuevosProductos.forEach((nuevoProducto)=>{
+        
+            productosNuevos = JSON.parse(this.localStorage.getItem("productosNuevos"));
+            console.log("revisando la local store 2")
+            console.log(productosNuevos)
+        
+            productosNuevos.forEach((nuevoProducto)=>{
                 productos.push(nuevoProducto);
             })
             localStorage.setItem("productos",JSON.stringify(productos));
@@ -283,33 +291,3 @@ function busquedaProducto(productoBuscar){
         }
     }
 });
-
-
-function uploadFile(file) {
-    var formData = new FormData();
-    formData.append('file', file);
-    formData.append('upload_preset', unsignerUploadPreset);
-
-    return new Promise((resolve, reject) => {
-        fetch('https://api.cloudinary.com/v1_1/' + cloudName + '/image/upload', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Error al cargar archivo a Cloudinary');
-        })
-        .then(data => {
-            console.log('Imagen subida exitosamente:', data);
-            var imageUrl = data.secure_url;
-            console.log('URL de la imagen:', imageUrl);
-            resolve(imageUrl);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            reject(error);
-        });
-    });
-}
