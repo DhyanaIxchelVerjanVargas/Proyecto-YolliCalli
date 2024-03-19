60e34818753ee41fb739067e0144f6fe945234e0
Edgar Ulises Benitez Cabriales <ulises131994@gmail.com>
2024-03-18 19:36:32 -0600

    Se realiza reset para correccion de un error

--- js/index.js
+++ js/index.js
@@ -1,157 +1,136 @@
-// Agregado para carrito
-let contenedorProdutos = document.getElementById("contenedorProductos");
-let botonProductoCarrito = document.getElementById("botonProductoCarrito");
-let modalEnvioCarrito = document.getElementById("modalEnvioCarrito");
-let btnEnvioCarrito = document.getElementById("btnEnvioCarrito");
-
-let productosCarrito = new Array();
-let nombreProd = "";
-let precioProd = 0;
-let imagenProd = "";
-let idProd = "";
-
-class ProductoCarrito{
-    nombre = "";
-    precio = 0;
-    imagen = "";
-    id = "";
-    cantidad = 0;
-    constructor(nombre,precio,imagen,id){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-        this.id = id;
-        this.cantidad = 1;
+let contenedorProductos = document.getElementById("contenedorProductos");
+let tarjetas = document.querySelectorAll('.carousel-inner .tarjetaCategoria');
+let carouselItems = document.querySelectorAll('.carousel-inner .carousel-item');
+let currentIndex = 0;
+let intervalId;
+let siguienteItem = document.getElementById("siguienteItem");
+let anteriorItem = document.getElementById("anteriorItem");
+let productos = [
+    { 
+        id: "CP1", 
+        nombre: "Cojín Tenango", 
+        imagen: "src/img/photo_2024-02-07_15-18-15 (2).jpg", 
+        precio: 400.00 
+    },
+    { 
+        id: "CP2", 
+        nombre: "Cojines Chiapanecos", 
+        imagen: "src/img/photo_2024-02-07_15-18-17.jpg", 
+        precio: 300.00 
+    },
+    { 
+        id: "CP3", 
+        nombre: "Camino de Mesa", 
+        imagen: "src/img/photo_2024-02-07_15-18-17 (2).jpg", 
+        precio: 500.00 
+    },
+    { 
+        id: "CP4", 
+        nombre: "Sagrado Corazón", 
+        imagen: "src/img/photo_2024-02-07_15-18-15 (3).jpg", 
+        precio: 200.00 
+    },
+    { 
+        id: "CP5", 
+        nombre: "Blusa Oaxaqueña", 
+        imagen: "src/img/photo_2024-02-07_18-45-03.jpg", 
+        precio: 400.00 
+    },
+    { 
+        id: "CP6", 
+        nombre: "Cesto Tejido", 
+        imagen: "src/img/photo_2024-02-07_18-45-12.jpg", 
+        precio: 450.00 
+    },
+    { 
+        id: "CP7", 
+        nombre: "Alebrije", 
+        imagen: "src/img/photo_2024-02-07_18-45-20.jpg", 
+        precio: 250.00 
+    },
+    { 
+        id: "CP8", 
+        nombre: "Cazuela de Barro", 
+        imagen: "src/img/photo_2024-02-07_18-45-24.jpg", 
+        precio: 100.00 
     }
-}// Class ProductoCarrito
+];
 
-cargarEventListeners();
-function cargarEventListeners(){
-    contenedorProdutos.addEventListener('click', obtenerDatos);
-}
-
-function obtenerDatos(e){
-    e.preventDefault();
-    if(e.target.classList.contains('borrarProducto')){
-        cardSeleccionada = e.target.parentElement.parentElement.parentElement.parentElement;
-        leerDatosCard(cardSeleccionada);
-    }
-}
-
-//Agregado para carrito
-function traerProductos(productosCarrito){
-    return productosCarrito = JSON.parse(localStorage.getItem("productosCarrito"));
-}
-
-function carritoVacio(){
-    return localStorage.getItem("productosCarrito") == null;
-}
-
-function agregarProducto(nombreProd,precioProd,imagenProd,idProd){
-    productosCarrito.push(new ProductoCarrito(nombreProd,precioProd,imagenProd,idProd));
-}
+// Resto del código
 
 
-function leerDatosCard(card){
-    nombreProd = card.querySelector(".card-title").innerHTML;
-    precioProd = (card.querySelector(".productPrecio").innerHTML).substring(2);
-    imagenProd = card.querySelector("img").src;
-    idProd = card.querySelector(".overlay").getAttribute("data-id");
-    modalEnvioCarrito.innerHTML=""
-    if(carritoVacio()){
-        agregarProducto(nombreProd,precioProd,imagenProd,idProd);
-        localStorage.setItem("productosCarrito", JSON.stringify(productosCarrito));
-        //console.log("Producto nuevo agregado al carrito VACÍO");
-    }else{
-        let isInCarrito = false;
-        productosCarrito = traerProductos(productosCarrito);
-        // Verificar que no esté anteriormente el producto y si ya está solo aumentar la cantidad
-        // console.log(productosCarrito[0].id);
-        productosCarrito.forEach((ProductoEnCarrito, index)=>{
-            if(ProductoEnCarrito.id == idProd){
-                //console.log("Entré");
-                ProductoEnCarrito.cantidad++;
-                localStorage.setItem("productosCarrito", JSON.stringify(productosCarrito));
-                isInCarrito = true;
+function ajustarTarjetasVisibles() {
+    if (window.innerWidth <= 425) {
+        tarjetas.forEach(function(tarjeta, index) {
+            tarjeta.style.display = index === currentIndex ? 'block' : 'none';
+            if (currentIndex < 3) {
+                carouselItems[0].classList.add("active");
+                carouselItems[1].classList.remove("active");
             }
-        })
-        if(!isInCarrito){
-            agregarProducto(nombreProd,precioProd,imagenProd,idProd);
-            //console.log("Producto nuevo agregado al carrito en No vacío");
-            localStorage.setItem("productosCarrito", JSON.stringify(productosCarrito));
-        }
+            if (currentIndex >= 3) {
+                carouselItems[1].classList.add("active");
+                carouselItems[0].classList.remove("active");
+            }
+        });
+    } else {
+        tarjetas.forEach(function(tarjeta) {
+            tarjeta.style.display = '';
+        });
     }
-    //Modal para envío exitoso a Carrito
-    modalEnvioCarrito.insertAdjacentHTML("beforeend", `
-    <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h1 class="modal-title fs-2" id="estadoModalLabel">YolliCalli</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerral"></button>
-        </div>
-        <div class="modal-body">
-        <p class="fs-3">Producto agregado al carrito</p>
-        </div>
-    </div>
-    </div>
-    `);
-    
-    btnEnvioCarrito.click();
 }
-// Termina agregado para carrito
 
-window.addEventListener('DOMContentLoaded', function() {
-    var tarjetas = document.querySelectorAll('.carousel-inner .tarjetaCategoria');
-    var currentIndex = 0;
-    var intervalId;
+function handleNext() {
+    clearInterval(intervalId);
+    currentIndex = currentIndex === tarjetas.length - 1 ? 0 : currentIndex + 1;
+    ajustarTarjetasVisibles();
+    reiniciarMovimientoAutomatico();
+}
 
-    function ajustarTarjetasVisibles() {
-        if (window.innerWidth <= 425) {
-            tarjetas.forEach(function(tarjeta, index) {
-                tarjeta.style.display = index === currentIndex ? 'block' : 'none';
-            });
-        } else {
-            tarjetas.forEach(function(tarjeta) {
-                tarjeta.style.display = ''; 
-            });
-        }
-    }
+function handlePrev() {
+    clearInterval(intervalId);
+    currentIndex = currentIndex === 0 ? tarjetas.length - 1 : currentIndex - 1;
+    ajustarTarjetasVisibles();
+    reiniciarMovimientoAutomatico();
+}
 
-    function handleNext() {
-        clearInterval(intervalId); 
-        currentIndex = currentIndex === tarjetas.length - 1 ? 0 : currentIndex + 1;
-        ajustarTarjetasVisibles();
-        reiniciarMovimientoAutomatico();
-    }
+function reiniciarMovimientoAutomatico() {
+    clearInterval(intervalId);
+    intervalId = setInterval(handleNext, 5000);
+}
 
-    function handlePrev() {
-        clearInterval(intervalId); 
-        currentIndex = currentIndex === 0 ? tarjetas.length - 1 : currentIndex - 1;
-        ajustarTarjetasVisibles();
-        reiniciarMovimientoAutomatico();
-    }
-
-    function reiniciarMovimientoAutomatico() {
-        
-        clearInterval(intervalId);
-        intervalId = setInterval(handleNext, 5000);
-    }
-
-    window.addEventListener('load', function() {
-        ajustarTarjetasVisibles();
-        reiniciarMovimientoAutomatico();
-    });
-    
-    window.addEventListener('resize', ajustarTarjetasVisibles);
-
-    document.querySelector('.carousel-control-next').addEventListener('click', function() {
-        handleNext();
-        ajustarTarjetasVisibles(); 
-    });
-    document.querySelector('.carousel-control-prev').addEventListener('click', function() {
-        handlePrev();
-        ajustarTarjetasVisibles(); 
-    });
-
-    reiniciarMovimientoAutomatico(); 
+window.addEventListener('load', function() {
+    ajustarTarjetasVisibles();
+    reiniciarMovimientoAutomatico();
 });
+
+window.addEventListener('resize', ajustarTarjetasVisibles);
+
+document.querySelector('.carousel-control-next').addEventListener('click', function(event) {
+    event.preventDefault();
+    handleNext();
+    ajustarTarjetasVisibles();
+    if (window.innerWidth > 425) {
+        siguienteItem.click();
+    }
+});
+
+document.querySelector('.carousel-control-prev').addEventListener('click', function(event) {
+    event.preventDefault();
+    handlePrev();
+    ajustarTarjetasVisibles();
+    if (window.innerWidth > 425) {
+        anteriorItem.click();
+    }
+});
+
+reiniciarMovimientoAutomatico();
+
+// Event listener para los botones "Más información"
+// contenedorProductos.addEventListener('click', function(event) {
+//     if (event.target.classList.contains('irProducto')) {
+//         const idCarta = event.target.closest('.cardProducto').id;
+//         localStorage.setItem("productos", JSON.stringify(productos));
+//         localStorage.setItem("idCarta", idCarta);
+//         window.location.href = "productoIndividual.html"; 
+//     }
+// });
